5a057d9c7d798590358e8a0cc3853e54c05037ed
estefaniania <64155925+qrppablo@users.noreply.github.com>
2020-08-10 19:03:43 +0200

    Updates

---
+++ precache-manifest.dd8da133a0cb519517cad8387de99924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a436a9831260d2d9b8da7c869f52ce85",
+    "url": "/github_api/index.html"
+  },
+  {
+    "revision": "c451bd66a346c18b9c38",
+    "url": "/github_api/static/css/main.bc0c0fef.chunk.css"
+  },
+  {
+    "revision": "eaf0201f8697271bf626",
+    "url": "/github_api/static/js/2.3b2f4574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github_api/static/js/2.3b2f4574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c451bd66a346c18b9c38",
+    "url": "/github_api/static/js/main.4db85958.chunk.js"
+  },
+  {
+    "revision": "1a924a025fb54f5f3db4",
+    "url": "/github_api/static/js/runtime-main.324d569e.js"
+  },
+  {
+    "revision": "7024fe935c23c4767a55e1f809e79d96",
+    "url": "/github_api/static/media/github_img.7024fe93.png"
+  }
+]);
